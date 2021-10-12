170dd195326f7f4cb3b1307515c714ee061d6c6f
Pablo Gatica <pablomartin.gatica.untdf@gmail.com>
2021-10-12 14:31:38 -0300

    Estructura complejo

---
+++ EstructuraComplejo/Complejo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraComplejo
+{
+     struct Complejo
+    {
+        public double Real { get; }
+        public double Imaginario { get; }
+
+        public Complejo (double real , double imaginario) : this()
+        {
+            Real = real; 
+            Imaginario = imaginario;
+
+        }
+
+        public double suma(Complejo otro)
+        {
+            return new Complejo(Real + otro.Real);
+        }
+
+    }
+}
---
+++ EstructuraComplejo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EstructuraComplejo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
